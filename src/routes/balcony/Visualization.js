60ab7c3d9a76ccc8dbf915ecdf71d1f472333026
Romane Dubois <romane.dubois@heig-vd.ch>
2019-04-09 10:59:48 +0200

    éléments html/css du balcon en place

--- src/routes/balcony/Visualization.js
+++ src/routes/balcony/Visualization.js
@@ -17,22 +17,36 @@ export default class Visualization extends React.Component {
                     Visualization for balcony n°{this.props.match.params.id}
                 </h1>*/}
          
-                <div id="balcony" >
+                <div id="balcony">
             <img src={Config.imgFolder + "/balconyXL.svg"} className="hidden" alt="Balcony" />
-                    <div className="pots" id="pot1">
+                    <div id="iconswrap">
+                        
+                        <img src={Config.imgFolder + "/icon/information.svg"} alt="plus d'informations" className="icons" />
+                        <img src={Config.imgFolder + "/icon/share.svg"} alt="partager" className="icons" />
+                        <img src={Config.imgFolder + "/icon/loading.svg"} alt="en cours d'enregistrement" className="icons hiddenIco" />
+                        <img src={Config.imgFolder + "/icon/tick-inside-circle.svg"} alt="Enregistré !" className="icons" />
+                         <img src={Config.imgFolder + "/icon/cancel.svg"} alt="Fermer" className="icons" />
+                    </div>
+                    <div className="pots balconyelement" id="pot1">
                     </div>    
-                    <div className="pots" id="pot2">
+                    <div className="pots balconyelement" id="pot2">
                     </div> 
-                    <div className="pots" id="pot3">
+                    <div className="pots balconyelement" id="pot3">
                     </div> 
-                    <div className="pots" id="pot4">
+                    <div className="pots balconyelement" id="pot4">
                     </div> 
-                    <div className="pots" id="pot5">
+                    <div className="pots balconyelement" id="pot5">
                     </div> 
-                    <div id="suspendedpot">
+                    <div className="balconyelement" id="suspendedpot">
                     </div> 
-                    <div id="climbingtrail">
+                    <div className="balconyelement" id="climbingtrail">
                     </div> 
+                    <div className="construction balconyelement" id="construction1">
+                    </div> 
+                    <div className="construction balconyelement" id="construction2">
+                    </div> 
+                </div>
+                <div className="helper w-100">
                 </div>
             </div>
                 
