a70ce1b68d037193fab21351982929a98b7d36e4
Irina Despot <irinadespot@gmail.com>
2019-04-12 10:05:57 +0200

    SignUp success message

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Map.css";
 import Config from "../Config";
 import $ from "jquery";
+import {Link} from "react-router-dom";
 
 export default class SignUp extends React.Component {
 
@@ -14,12 +15,14 @@ export default class SignUp extends React.Component {
             userName: "",
             password1: "",
             password2: "",
-            error: null
+            error: null,
+            showMessage: false
         };
 
         this.messages = {
             emptyField: "L'adresse email, le nom d'utilisateur et le mot de passe sont obligatoires.",
-            passwordsDontMatch: "Les mots de passe ne correspondent pas."
+            passwordsDontMatch: "Les mots de passe ne correspondent pas.",
+            userExists: "Cette adresse email ou ce nom d'utilisateur existent déjà."
         };
 
         this.register = this.register.bind(this);
@@ -39,7 +42,7 @@ export default class SignUp extends React.Component {
     }
 
     register() {
-        if(!this.checkErrors()) return null;
+        if (!this.checkErrors()) return null;
 
         let data = {
             username: this.state.userName,
@@ -58,7 +61,9 @@ export default class SignUp extends React.Component {
             data: JSON.stringify(data),
             context: this
         }).done(res => {
-            if(res.code === 200) this.login();
+            if (res.code === 200) this.login();
+        }).fail(() => {
+            this.setState({error: this.messages.userExists});
         });
     }
 
@@ -74,9 +79,10 @@ export default class SignUp extends React.Component {
             data: JSON.stringify(data),
             context: this
         }).done(res => {
-            if(res.token) {
+            if (res.token) {
                 localStorage.setItem("authKey", res.token);
-                this.props.history.push("/dashboard");
+                this.setState({showMessage: true});
+                //this.props.history.push("/dashboard");
             }
         });
     }
@@ -99,56 +105,80 @@ export default class SignUp extends React.Component {
                 <i className="fas fa-times text-light" onClick={this.props.onClose}
                    style={closeIconStyle} title="Fermer"/>
                 <div className="row">
-                    <form className="col-12 col-lg-6 mx-auto p-4">
-                        <h1 className="text-light text-center">Inscrivez-vous en quelques clics!</h1>
-                        <div className="row">
-                            <div className="col-12 col-sm-6 py-2">
-                                <input type="text" className="form-control" placeholder="Prénom"
-                                       value={this.state.firstName}
-                                       onChange={e => this.setState({firstName: e.target.value})}/>
-                            </div>
-                            <div className="col-12 col-sm-6 py-2">
-                                <input type="text" className="form-control" placeholder="Nom"
-                                       value={this.state.lastName}
-                                       onChange={e => this.setState({lastName: e.target.value})}/>
-                            </div>
-                            <div className="col-12 py-2">
-                                <input type="text" className="form-control" placeholder="Adresse email"
-                                       value={this.state.email}
-                                       onChange={e => this.setState({email: e.target.value})}/>
-                            </div>
-                            <div className="col-12 py-2">
-                                <input type="text" className="form-control" placeholder="Nom d'utilisateur"
-                                       value={this.state.userName}
-                                       onChange={e => this.setState({userName: e.target.value})}/>
-                            </div>
-                            <div className="col-12 col-sm-6 py-2">
-                                <input type="password" className="form-control" placeholder="Mot de passe"
-                                       value={this.state.password1}
-                                       onChange={e => this.setState({password1: e.target.value})}/>
-                            </div>
-                            <div className="col-12 col-sm-6 py-2">
-                                <input type="password" className="form-control" placeholder="Répéter le mot de passe"
-                                       value={this.state.password2}
-                                       onChange={e => this.setState({password2: e.target.value})}/>
-                            </div>
-                        </div>
-                        {this.state.error && (
-                            <div className="row py-2">
-                                <div className="col-12">
-                                    <div className="alert alert-danger w-100">{this.state.error}</div>
-                                </div>
-                            </div>
-                        )}
-                        <div className="row mt-2">
-                            <div className="col-12">
-                                <button type="button" className="btn btn-success w-100" onClick={this.register}>
-                                    C'est parti !
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    {this.state.showMessage ? this.renderRegisterMessage() : this.renderForm()}
                 </div>
+            </div>
+        );
+    }
+
+    renderForm() {
+        return (
+            <form className="col-12 col-lg-6 mx-auto p-4">
+                <h1 className="text-light text-center">Inscrivez-vous en quelques clics!</h1>
+                <div className="row">
+                    <div className="col-12 col-sm-6 py-2">
+                        <input type="text" className="form-control" placeholder="Prénom"
+                               value={this.state.firstName}
+                               onChange={e => this.setState({firstName: e.target.value})}/>
+                    </div>
+                    <div className="col-12 col-sm-6 py-2">
+                        <input type="text" className="form-control" placeholder="Nom"
+                               value={this.state.lastName}
+                               onChange={e => this.setState({lastName: e.target.value})}/>
+                    </div>
+                    <div className="col-12 py-2">
+                        <input type="text" className="form-control" placeholder="Adresse email"
+                               value={this.state.email}
+                               onChange={e => this.setState({email: e.target.value})}/>
+                    </div>
+                    <div className="col-12 py-2">
+                        <input type="text" className="form-control" placeholder="Nom d'utilisateur"
+                               value={this.state.userName}
+                               onChange={e => this.setState({userName: e.target.value})}/>
+                    </div>
+                    <div className="col-12 col-sm-6 py-2">
+                        <input type="password" className="form-control" placeholder="Mot de passe"
+                               value={this.state.password1}
+                               onChange={e => this.setState({password1: e.target.value})}/>
+                    </div>
+                    <div className="col-12 col-sm-6 py-2">
+                        <input type="password" className="form-control" placeholder="Répéter le mot de passe"
+                               value={this.state.password2}
+                               onChange={e => this.setState({password2: e.target.value})}/>
+                    </div>
+                </div>
+                {this.state.error && (
+                    <div className="row py-2">
+                        <div className="col-12">
+                            <div className="alert alert-danger w-100">{this.state.error}</div>
+                        </div>
+                    </div>
+                )}
+                <div className="row mt-2">
+                    <div className="col-12">
+                        <button type="button" className="btn btn-success w-100" onClick={this.register}>
+                            C'est parti !
+                        </button>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+
+    renderRegisterMessage() {
+        return (
+            <div className="col-12 col-md-8 col-lg-6 mx-auto text-light text-center">
+                <h1 className="text-light">En route pour la Biodi-vers-City !</h1>
+                <h3 className="text-light">Bienvenue dans une ville grise, triste, qui a besoin de vous pour être la
+                    plus verte !</h3>
+                <p>Félicitations ! En vous inscrivant, vous venez de faire gagner 200 points à votre quartier dans une
+                    compétition entre les cinq quartier de Lausanne pour être le plus bio-divers de la ville.</p>
+                <p>Ici, vous pourrez imaginer vos balcons respectant le plus la biodiversité et vous informer sur
+                    comment le construire et l'entretenir. Ce projet est une initiative de BirdLife, qui oeuvre pour la
+                    préservation des espèces naturelles, de plus en plus menacées par notre urbanisation de plus en plus
+                    excessive. Il est temps d’agir, et chaque geste compte. En espérant vous aider à trouver quel geste
+                    réaliser, bonne visite de Biodi-vers-City !</p>
+                <Link className="btn btn-primary w-100 my-3" to="/dashboard">Continuer</Link>
             </div>
         );
     }
